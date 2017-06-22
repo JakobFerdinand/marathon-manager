1bd057fa63ece0d03be30535a1214f723755129b
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-06-22 12:06:48 +0200

    Start run

--- Core/Repositories/ICategoryRepository.cs
+++ Core/Repositories/ICategoryRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Core.Models;
+using System.Collections.Generic;
 
 namespace Core.Repositories
 {
     public interface ICategoryRepository : IRepository<Category>
     {
-
+        IEnumerable<Category> GetNotStarted();
     }
 }
---
+++ UI.StartRuns/Program.cs
@@ -0,0 +1,16 @@
+﻿using Logic.DIConfiguration;
+using System;
+
+namespace UI.StartRuns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new Container();
+
+            var starter = container.GetInstance<RunStarter>();
+            starter.Run();
+        }
+    }
+}
