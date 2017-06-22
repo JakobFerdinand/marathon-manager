30c7f455f05c6835f33766198594ff81e7bedf55
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-06-22 11:36:24 +0200

    Add Database again

---
+++ Database/Migrations/0002_20170622-1135_jw2.sql
@@ -0,0 +1,22 @@
+﻿-- <Migration ID="285406ef-2bd4-41fd-a2b6-2629df0abf04" />
+GO
+
+PRINT N'Creating [dbo].[Runners]'
+GO
+CREATE TABLE [dbo].[Runners]
+(
+[RunnerId] [int] NOT NULL IDENTITY(1, 1),
+[Startnumber] [int] NULL,
+[Firstname] [nvarchar] (50) NOT NULL,
+[Lastname] [nvarchar] (50) NOT NULL,
+[YearOfBirth] [int] NOT NULL,
+[ChipId] [nchar] (10) NULL,
+[TimeAtDestination] [datetime2] NULL,
+[RunningTime] [time] NULL,
+[CategoryId] [int] NOT NULL
+)
+GO
+PRINT N'Creating primary key [PK_Runners] on [dbo].[Runners]'
+GO
+ALTER TABLE [dbo].[Runners] ADD CONSTRAINT [PK_Runners] PRIMARY KEY CLUSTERED  ([RunnerId])
+GO
