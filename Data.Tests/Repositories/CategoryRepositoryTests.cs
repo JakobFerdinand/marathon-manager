3f70c27a81e908fd1f595c35c29fbe322d14b345
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-22 18:19:46 +0200

    CRUD Tests for DbContext

---
+++ Data.Tests/Repositories/CategoryRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using Data.Repositories;
+using System;
+using Xunit;
+
+namespace Data.Tests.Repositories
+{
+    public class CategoryRepositoryTests
+    {
+        [Fact]
+        public void Constructor_DbContext_Null_ArgumentNullException() => Assert.Throws<ArgumentNullException>(() => new CategoryRepository(null));
+    }
+}
