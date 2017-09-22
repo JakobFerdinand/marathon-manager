aa9b13b51df695c4c2bed641b1800ee1cade630e
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-22 17:46:13 +0200

    Add Tests for Extension Methods

---
+++ Logic.Common.Tests/Extensions/IConfigurationExtentionsTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using Logic.Common.Extensions;
+using Xunit;
+
+namespace Logic.Common.Tests
+{
+    public class IConfigurationExtentionsTests
+    {
+        [Fact]
+        public void GetLoggingPath_calles_GetSection_for_Logging_with_given_Name()
+        {
+            var configuration = Substitute.For<IConfiguration>();
+            var configurationSection = Substitute.For<IConfigurationSection>();
+            configuration.GetSection(null).ReturnsForAnyArgs(configurationSection);
+            configurationSection["the path to log to"].Returns("worked");
+
+            var result = configuration.GetLoggingPath("the path to log to");
+
+            configuration.Received().GetSection("Logging");
+            Assert.Equal("worked", result);
+        }
+    }
+}
---
+++ Logic.Common.Tests/Extensions/IContainerExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Logic.Common.Extensions;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using StructureMap;
+using System;
+using Xunit;
+
+namespace Logic.Common.Tests.Extensions
+{
+    public class IContainerExtensionsTests
+    {
+        [Fact]
+        public void RegisterConcreteTypeAsSingelton_registers_a_Type_as_Singelton()
+        {
+            var container = Substitute.For<IContainer>();
+
+            container.RegisterConcreteTypeAsSingelton<TheTypeToRegister>();
+
+            container.WhenForAnyArgs(c => c.Configure(null)).Do(i => i.Arg<ConfigurationExpression>().Received().ForConcreteType<TheTypeToRegister>());
+            container.ReceivedWithAnyArgs().Configure(null);
+        }
+        [Fact]
+        public void AddDbContext_registers_the_Context_with_options()
+        {
+            var container = Substitute.For<IContainer>();
+
+            DbContextOptionsBuilder<TheContextToRegister> builder = null;
+            Action<DbContextOptionsBuilder<TheContextToRegister>> options = b => builder = b;
+
+            container.AddDbContext(options);
+
+            Assert.NotNull(builder);
+            container.ReceivedWithAnyArgs().Configure(null);
+            container.WhenForAnyArgs(c => c.Configure(null)).Do(i => i.Arg<ConfigurationExpression>().Received().ForConcreteType<TheContextToRegister>());
+            container.WhenForAnyArgs(c => c.Configure(null)).Do(i => i.Arg<ConfigurationExpression>().ForConcreteType<TheContextToRegister>().Configure.Received().Ctor<DbContextOptions<TheContextToRegister>>());
+            container.WhenForAnyArgs(c => c.Configure(null)).Do(i => i.Arg<ConfigurationExpression>().ForConcreteType<TheContextToRegister>().Configure.Ctor<DbContextOptions<TheContextToRegister>>().Received().Is(builder.Options));
+        }
+
+        class TheTypeToRegister { }
+        class TheContextToRegister : DbContext { }
+    }
+}
