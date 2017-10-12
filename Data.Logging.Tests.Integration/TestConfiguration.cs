28829597d0921499f6dd8de27e295a2a2a7be19d
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-12 14:50:04 +0200

    Add ChangeLog Integration Tests for SqlServer

---
+++ Data.Logging.Tests.Integration/TestConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Data.Logging.Tests.Integration
+{
+    internal static class TestConfiguration
+    {
+        public static string ConnectionString => "server=localhost;database={0};integrated security=true";
+    }
+}
