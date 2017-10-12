50dbd70714af3475a3857f6bbe56e0da8e40291f
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-12 12:36:12 +0200

    Category Configuration Integration Tests for SqlServer

--- Data.Tests.Integration/Configurations/CategoryConfigurationTests.cs
+++ Data.Tests.Integration/Configurations/CategoryConfigurationTests.cs
@@ -104,7 +104,7 @@ SELECT c.name as ColumnName
     JOIN sys.columns c ON t.object_id = c.object_id
     JOIN sys.types y ON y.system_type_id = c.system_type_id
     WHERE t.name = 'Categories'";
-    
+
                     using (var reader = await command.ExecuteReaderAsync())
                     {
                         var results = new List<(string columnName, string typeName)>();
@@ -174,6 +174,133 @@ SELECT COLUMN_NAME
 
                         Assert.True(containsWantedColum);
                     }
+                }
+            }
+
+            using (var context = new RunnerDbContext(options))
+                await context.Database.EnsureDeletedAsync();
+        }
+        [Fact]
+        [Trait("Integration", "Sql Server")]
+        public async Task Categories_should_have_a_nullable_column_called_Starttime_of_type_datetimte2()
+        {
+            var databaseName = nameof(Categories_should_have_a_nullable_column_called_Starttime_of_type_datetimte2);
+            var connectionString = string.Format(TestConfiguration.ConnectionString, databaseName);
+            var options = new DbContextOptionsBuilder<RunnerDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+            using (var context = new RunnerDbContext(options))
+                await context.Database.EnsureCreatedAsync();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+SELECT COLUMN_NAME
+    , DATA_TYPE
+    , IS_NULLABLE
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'Categories'";
+
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        var results = new List<(string columnName, string typeName, bool isNullable)>();
+                        while (await reader.ReadAsync())
+                        {
+                            var columnName = reader["COLUMN_NAME"] as string;
+                            var typeName = reader["DATA_TYPE"] as string;
+                            var isNullable = (reader["IS_NULLABLE"] as string).Equals("YES", StringComparison.InvariantCultureIgnoreCase);
+
+                            results.Add((columnName, typeName, isNullable));
+                        }
+
+                        var containsWantedColum = results.Any(r => r.columnName.Equals("Starttime", StringComparison.InvariantCultureIgnoreCase) &&
+                                                                   r.typeName.Equals("datetime2", StringComparison.InvariantCultureIgnoreCase) &&
+                                                                   r.isNullable == true);
+
+                        Assert.True(containsWantedColum);
+                    }
+                }
+            }
+
+            using (var context = new RunnerDbContext(options))
+                await context.Database.EnsureDeletedAsync();
+        }
+        [Fact]
+        [Trait("Integration", "Sql Server")]
+        public async Task Categories_should_have_a_not_nullable_column_called_PlannedStarttime_of_type_datetimte2()
+        {
+            var databaseName = nameof(Categories_should_have_a_not_nullable_column_called_PlannedStarttime_of_type_datetimte2);
+            var connectionString = string.Format(TestConfiguration.ConnectionString, databaseName);
+            var options = new DbContextOptionsBuilder<RunnerDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+            using (var context = new RunnerDbContext(options))
+                await context.Database.EnsureCreatedAsync();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+SELECT COLUMN_NAME
+    , DATA_TYPE
+    , IS_NULLABLE
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'Categories'";
+
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        var results = new List<(string columnName, string typeName, bool isNullable)>();
+                        while (await reader.ReadAsync())
+                        {
+                            var columnName = reader["COLUMN_NAME"] as string;
+                            var typeName = reader["DATA_TYPE"] as string;
+                            var isNullable = (reader["IS_NULLABLE"] as string).Equals("YES", StringComparison.InvariantCultureIgnoreCase);
+
+                            results.Add((columnName, typeName, isNullable));
+                        }
+
+                        var containsWantedColum = results.Any(r => r.columnName.Equals("PlannedStarttime", StringComparison.InvariantCultureIgnoreCase) &&
+                                                                   r.typeName.Equals("datetime2", StringComparison.InvariantCultureIgnoreCase) &&
+                                                                   r.isNullable == false);
+
+                        Assert.True(containsWantedColum);
+                    }
+                }
+            }
+
+            using (var context = new RunnerDbContext(options))
+                await context.Database.EnsureDeletedAsync();
+        }
+        [Fact]
+        [Trait("Integration", "Sql Server")]
+        public async Task Categories_should_have_4_columns()
+        {
+            var databaseName = nameof(Categories_should_have_4_columns);
+            var connectionString = string.Format(TestConfiguration.ConnectionString, databaseName);
+            var options = new DbContextOptionsBuilder<RunnerDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+            using (var context = new RunnerDbContext(options))
+                await context.Database.EnsureCreatedAsync();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"
+SELECT Count(*)
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'Categories'";
+
+                    var countColumns = (int)await command.ExecuteScalarAsync();
+
+                    Assert.Equal(4, countColumns);
                 }
             }
 
