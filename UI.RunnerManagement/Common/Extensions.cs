d2ab5728713225a1ef1f5919aead68473fbcde9f
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2018-09-07 18:06:38 +0200

    Add new Runner

---
+++ UI.RunnerManagement/Common/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace UI.RunnerManagement.Common
+{
+    internal static class Extensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> @this)
+            => new ObservableCollection<T>(@this);
+    }
+}
