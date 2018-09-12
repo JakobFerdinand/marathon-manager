20ab2803a105b1cf68e5a285d51e0563540cde02
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2018-09-12 19:32:15 +0200

    ScrollToSelectedItemBehavior

---
+++ UI.RunnerManagement/Behaviors/ScrollToSelectedItemBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace UI.RunnerManagement.Behaviors
+{
+    public class ScrollToSelectedItemBehavior : Behavior<DataGrid>
+    {
+        protected override void OnAttached()
+            => AssociatedObject.SelectionChanged += SelectionChangedHandler;
+
+        protected override void OnDetaching()
+            => AssociatedObject.SelectionChanged -= SelectionChangedHandler;
+
+        private void SelectionChangedHandler(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is DataGrid dataGrid && dataGrid.SelectedItem != null)
+                dataGrid.Dispatcher.Invoke(() =>
+                {
+                    dataGrid.UpdateLayout();
+                    dataGrid.ScrollIntoView(dataGrid.SelectedItem);
+                });
+        }
+    }
+}
