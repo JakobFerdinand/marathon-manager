60d605e886cbe96f6f44dcc5b40881543485f5ef
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-09-17 08:18:31 +0200

    Create Project ExportResults

---
+++ UI.ExportResults/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UI.ExportResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
