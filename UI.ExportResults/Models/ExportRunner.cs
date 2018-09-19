ea51d674db61aab854ae7a44cd609b4e95535ab6
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-09-19 17:27:46 +0200

    Create ExportRunner

---
+++ UI.ExportResults/Models/ExportRunner.cs
@@ -0,0 +1,29 @@
+﻿using Core.Models;
+using System;
+
+namespace UI.ExportResults.Models
+{
+    public class ExportRunner
+    {
+        public ExportRunner(int rang, int startnummer, string vorname, string nachname, Gender geschlecht, string verein, int geburtsjahr, TimeSpan zeit)
+        {
+            Rang = rang;
+            Startnummer = startnummer;
+            Vorname = vorname;
+            Nachname = nachname;
+            Geschlecht = geschlecht;
+            Verein = verein;
+            Geburtsjahr = geburtsjahr;
+            Zeit = zeit;
+        }
+
+        public int Rang { get; }
+        public int Startnummer { get; }
+        public string Vorname { get; }
+        public string Nachname { get; }
+        public Gender Geschlecht { get; }
+        public string Verein { get; }
+        public int Geburtsjahr { get; }
+        public TimeSpan Zeit { get; }
+    }
+}
