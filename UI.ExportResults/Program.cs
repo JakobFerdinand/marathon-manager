768f710ea5566c0ae5a7f13137fd43cc66a1fbbd
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-09-19 18:12:37 +0200

    Export Runners add Configuration

--- UI.ExportResults/Program.cs
+++ UI.ExportResults/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace UI.ExportResults
 {
     class Program
     {
+        private static IConfigurationRoot Configuration { get; set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static void Startup()
+        {
+            var basePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
         }
     }
 }
