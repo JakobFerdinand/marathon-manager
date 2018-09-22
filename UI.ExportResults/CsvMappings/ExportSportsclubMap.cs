bdd2b778beac7259243f5c6bc35975312399e29a
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2018-09-22 16:19:58 +0200

    ExportResults Count Sportsclubs

---
+++ UI.ExportResults/CsvMappings/ExportSportsclubMap.cs
@@ -0,0 +1,15 @@
+﻿using CsvHelper.Configuration;
+using UI.ExportResults.Models;
+
+namespace UI.ExportResults.CsvMappings
+{
+    public class ExportSportsclubMap : ClassMap<ExportSportsclub>
+    {
+        public ExportSportsclubMap()
+        {
+            Map(s => s.Rang).Name("Rang");
+            Map(s => s.Name).Name("Verein");
+            Map(s => s.Count).Name("Anzahl");
+        }
+    }
+}
---
+++ UI.ExportResults/Models/ExportSportsclub.cs
@@ -0,0 +1,16 @@
+﻿namespace UI.ExportResults.Models
+{
+    public sealed class ExportSportsclub
+    {
+        public ExportSportsclub(int rang, string name, int count)
+        {
+            Rang = rang;
+            Name = name;
+            Count = count;
+        }
+
+        public int Rang { get; }
+        public string Name { get; }
+        public int Count { get; }
+    }
+}
