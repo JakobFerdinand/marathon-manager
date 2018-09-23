7a40348045d1da1d94183403999dfb092458ba7f
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2018-09-23 07:50:14 +0200

    Prepare for Excel Export

---
+++ UI.ExportResults/ExcelExport.cs
@@ -0,0 +1,18 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UI.ExportResults
+{
+    public static class ExcelExport
+    {
+        public static void Export<T>(string path, IEnumerable<T> data)
+        {
+            using (var p = new ExcelPackage(new FileInfo(path)))
+            {
+                var ws = p.Workbook.Worksheets.Add("Alle Läufer");
+
+            }
+        }
+    }
+}
