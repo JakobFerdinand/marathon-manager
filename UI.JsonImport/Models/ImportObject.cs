31e50633aa0e88f3597bad637a288c08c2e1ca01
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-06-10 14:14:20 +0200

    Update ImportObject for 2019

--- UI.JsonImport/Models/ImportObject.cs
+++ UI.JsonImport/Models/ImportObject.cs
@@ -2,38 +2,20 @@
 {
     internal class ImportObject
     {
-        public string Id { get; set; }
-        public string Spalte_01 { get; set; }
-        public string Spalte_02 { get; set; }
-        public string Spalte_03 { get; set; }
-        public string Spalte_04 { get; set; }
-        public string Spalte_05 { get; set; }
-        public string Spalte_06 { get; set; }
-        public string Spalte_07 { get; set; }
-        public string Spalte_08 { get; set; }
-        public string Spalte_09 { get; set; }
-        public string Spalte_10 { get; set; }
-        public string Spalte_11 { get; set; }
-        public string Spalte_12 { get; set; }
-        public string Spalte_13 { get; set; }
-        public string Spalte_14 { get; set; }
-        public string Spalte_15 { get; set; }
-        public string Spalte_16 { get; set; }
-        public string Spalte_17 { get; set; }
-        public string Spalte_18 { get; set; }
-        public string Spalte_19 { get; set; }
-        public string Spalte_20 { get; set; }
-        public string Spalte_21 { get; set; }
-        public string Spalte_22 { get; set; }
-        public string Spalte_23 { get; set; }
-        public string Spalte_24 { get; set; }
-        public string Spalte_25 { get; set; }
-        public string Spalte_26 { get; set; }
-        public string Spalte_27 { get; set; }
-        public string Spalte_28 { get; set; }
-        public string Spalte_29 { get; set; }
-        public string Spalte_30 { get; set; }
-        public string Spalte_31 { get; set; }
-        public string time { get; set; }
+        public int _ { get; set; }
+        public string DatumderEinreichung { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Verein { get; set; }
+        public string Email { get; set; }
+        public string Geschlecht { get; set; }
+        public string Geburtsdatum { get; set; }
+        public string Wohnort { get; set; }
+        public string Bewerb { get; set; }
+        public string Zeit { get; set; }
+        public string PlatzierungBewerb { get; set; }
+        public string AK { get; set; }
+        public string PlatzierungAK { get; set; }
     }
+
 }
