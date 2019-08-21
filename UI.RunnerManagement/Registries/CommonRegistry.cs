ac94cc1b6df508984b78278cba590d9d4da5751f
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-08-21 22:30:38 +0200

    Administration page initial open read connectionstring from appsettings #21

--- UI.RunnerManagement/Registries/CommonRegistry.cs
+++ UI.RunnerManagement/Registries/CommonRegistry.cs
@@ -26,10 +26,6 @@ namespace UI.RunnerManagement.Registries
             For<INotificationService>()
                 .Use(c => new NotificationService(new NotificationManager(null)))
                 .Singleton();
-
-            For<IConnectionstringService>()
-                .Use<ConnectionstringService>()
-                .Singleton();
         }
     }
 }
