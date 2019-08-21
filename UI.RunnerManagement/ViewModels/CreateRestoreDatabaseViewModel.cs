1dabc41a4a385a2d26a8ddc8a964368d468b3c25
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-08-21 10:18:58 +0200

    CreateRestoreDatabase list available servers #21

--- UI.RunnerManagement/ViewModels/CreateRestoreDatabaseViewModel.cs
+++ UI.RunnerManagement/ViewModels/CreateRestoreDatabaseViewModel.cs
@@ -1,14 +1,36 @@
 ﻿using Core;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using UI.RunnerManagement.Common;
 
 namespace UI.RunnerManagement.ViewModels
 {
-    public class CreateRestoreDatabaseViewModel
+    public class CreateRestoreDatabaseViewModel : ViewModelBase
     {
         private readonly IUnitOfWork unitOfWork;
 
         public CreateRestoreDatabaseViewModel(IUnitOfWork unitOfWork)
         {
             this.unitOfWork = unitOfWork ?? throw new System.ArgumentNullException(nameof(unitOfWork));
+            RefreshServersCommand = new Command(RefreshServers);
+        }
+
+        public ObservableCollection<string> AvailableServers { get; } = new ObservableCollection<string>();
+
+        private string server;
+        public string Server
+        {
+            get => server;
+            set => Set(ref server, value);
+        }
+
+        public ICommand RefreshServersCommand { get; }
+        private void RefreshServers()
+        {
+            AvailableServers.Clear();
+            var servers = unitOfWork.Database.GetAvailableServers();
+            AvailableServers.AddRange(servers);
         }
     }
 }
