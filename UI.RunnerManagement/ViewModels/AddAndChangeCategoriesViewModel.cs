8c4b25a07958e1cf61170337e14f1cf4dd174ba2
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-08-22 20:27:26 +0200

    AddAndChangeCategories display categories #21

--- UI.RunnerManagement/ViewModels/AddAndChangeCategoriesViewModel.cs
+++ UI.RunnerManagement/ViewModels/AddAndChangeCategoriesViewModel.cs
@@ -1,6 +1,44 @@
-﻿namespace UI.RunnerManagement.ViewModels
+﻿using Core;
+using Core.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using UI.RunnerManagement.Common;
+
+namespace UI.RunnerManagement.ViewModels
 {
-    public class AddAndChangeCategoriesViewModel
+    public class AddAndChangeCategoriesViewModel : ViewModelBase
     {
+        private readonly IUnitOfWork unitOfWork;
+
+        public AddAndChangeCategoriesViewModel(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            ReloadCategoriesCommand = new Command(ReloadCategoriesCommandHandler);
+        }
+
+        public ICommand ReloadCategoriesCommand { get; }
+        public ObservableCollection<Category> Categories { get; } = new ObservableCollection<Category>();
+
+        private Category selectedCategory;
+        public Category SelectedCategory
+        {
+            get => selectedCategory;
+            set
+            {
+                selectedCategory = value;
+                if (selectedCategory != null)
+                    unitOfWork.Attach(selectedCategory);
+                RaisePropertyChanged();
+            }
+        }
+
+        private void ReloadCategoriesCommandHandler()
+        {
+            SelectedCategory = null;
+            var categories = unitOfWork.Categories.GetAll(asNoTracking: true);
+            Categories.Clear();
+            Categories.AddRange(categories);
+        }
     }
 }
--- UI.RunnerManagement/Views/AddAndChangeCategoriesView.xaml.cs
+++ UI.RunnerManagement/Views/AddAndChangeCategoriesView.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UI.RunnerManagement.Views
 {
@@ -20,9 +8,13 @@ namespace UI.RunnerManagement.Views
     /// </summary>
     public partial class AddAndChangeCategoriesView : UserControl
     {
-        public AddAndChangeCategoriesView()
+        public AddAndChangeCategoriesView() => InitializeComponent();
+
+        private void categoriesDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            InitializeComponent();
+            nameTextBox.Focus();
+            if (!nameTextBox.Text.IsNullOrEmpty())
+                nameTextBox.SelectAll();
         }
     }
 }
