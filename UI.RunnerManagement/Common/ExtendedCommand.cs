06c4ae545c8e80a672d677f994004e27d0b93ae6
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2019-08-22 19:36:49 +0200

    disable save in CreateRestoreDatabaseView #21

---
+++ UI.RunnerManagement/Common/ExtendedCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UI.RunnerManagement.Common
+{
+    public class ExtendedCommand : Command
+    {
+        private readonly Func<string> getTooltip;
+        private readonly Func<string> getCannotExecuteTooltip;
+
+        public ExtendedCommand(Action execute, Func<string> getTooltip)
+            : base(execute)
+            => this.getTooltip = getTooltip ?? throw new ArgumentNullException(nameof(getTooltip));
+
+        public ExtendedCommand(Action execute, Func<string> getTooltip, Func<bool> canExecute, Func<string> getCannotExecuteTooltip) : base(execute, canExecute)
+        {
+            this.getTooltip = getTooltip ?? throw new ArgumentNullException(nameof(getTooltip));
+            this.getCannotExecuteTooltip = getCannotExecuteTooltip ?? throw new ArgumentNullException(nameof(getCannotExecuteTooltip));
+        }
+
+        public string Tooltip => CanExecute(null)
+            ? getTooltip()
+            : getCannotExecuteTooltip?.Invoke() ?? getTooltip();
+    }
+}
