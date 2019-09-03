0f5b54e2f47cf6fcd958c0604e108226ca5c251f
Jakob Ferdinand Wegenschimmel <jwegenschimmel@rzl.at>
2019-09-03 09:15:39 +0200

    update publish_win10.bat output variable

--- publish_win10.bat
+++ publish_win10.bat
@@ -4,7 +4,7 @@ IF "%configurationName%" == "" /p configurationName=Release ConfigurationName: %
 cd UI.JsonImport
 dotnet publish -c %configurationName% -r win10-x64
 set outputPath=.\bin\%configurationName%\netcoreapp2.2\win10-x64\
-if exist ".\bin\AnyCpu\" ( 
+if exist .\bin\AnyCpu\ ( 
     outputPath=.\bin\AnyCpu\%configurationName%\netcoreapp2.2\win10-x64\
 )
 robocopy %outputPath% ..\Publish\UI.JsonImport\ /E
@@ -13,18 +13,30 @@ cd ..
 
 cd UI.StartRuns
 dotnet publish -c %configurationName% -r win10-x64
+set outputPath=.\bin\%configurationName%\netcoreapp2.2\win10-x64\
+if exist .\bin\AnyCpu\ ( 
+    outputPath=.\bin\AnyCpu\%configurationName%\netcoreapp2.2\win10-x64\
+)
 robocopy %outputPath% ..\Publish\UI.StartRuns\ /E
 
 cd ..
 
 cd UI.TimeRecord
 dotnet publish -c %configurationName% -r win10-x64
+set outputPath=.\bin\%configurationName%\netcoreapp2.2\win10-x64\
+if exist .\bin\AnyCpu\ ( 
+    outputPath=.\bin\AnyCpu\%configurationName%\netcoreapp2.2\win10-x64\
+)
 robocopy %outputPath% ..\Publish\UI.TimeRecord\ /E
 
 cd ..
 
 cd UI.ExportResults
 dotnet publish -c %configurationName% -r win10-x64
+set outputPath=.\bin\%configurationName%\netcoreapp2.2\win10-x64\
+if exist .\bin\AnyCpu\ ( 
+    outputPath=.\bin\AnyCpu\%configurationName%\netcoreapp2.2\win10-x64\
+)
 robocopy %outputPath% ..\Publish\UI.ExportResults\ /E
 
 cd ..
