d831479b227dcaac209306df3f4bc945e368dd9a
Jakob Ferdinand Wegenschimmel <jwegenschimmel@rzl.at>
2019-09-03 11:22:44 +0200

    change publish scripts to powershell

--- pack_artifacts.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set configurationName=%1
-IF "%configurationName%" == "" /p configurationName=Release ConfigurationName: %=%
-
-call .\publish_win10.bat %configurationName%
-
-robocopy .\MarathonManager\bin\%configurationName%\ .\Publish\MarathonManager\ /E
