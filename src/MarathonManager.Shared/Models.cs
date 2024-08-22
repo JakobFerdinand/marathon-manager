9081c5757a4aa00f23247f047ce1459db2b2434d
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2024-08-22 09:24:27 +0200

    Create basic Blazor setup

---
+++ src/MarathonManager.Shared/Models.cs
@@ -0,0 +1,8 @@
+namespace MarathonManager.Shared;
+
+public sealed class Runner
+{
+    public required string Firstname { get; set; }
+    public required string Lastname { get; set; }
+    public required int YearOfBirth { get; set; }
+}
---
+++ src/MarathonManager/Program.cs
@@ -0,0 +1,40 @@
+using MarathonManager.Components;
+using MarathonManager.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+});
+
+builder.Services.AddTransient<IQueueService, QueueService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
