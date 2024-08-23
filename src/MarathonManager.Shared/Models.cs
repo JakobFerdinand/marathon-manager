cf56a459f40f1b485889afa5812c88d9eb9fde82
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2024-08-23 11:28:57 +0200

    change to blazor swa

--- src/MarathonManager.Shared/Models.cs
+++
@@ -1,8 +0,0 @@
-namespace MarathonManager.Shared;
-
-public sealed class Runner
-{
-    public required string Firstname { get; set; }
-    public required string Lastname { get; set; }
-    public required int YearOfBirth { get; set; }
-}
